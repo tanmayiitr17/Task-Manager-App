dae42f18273e4287dfa3df39a2e556f716d12f33
Tanmay Pandey <tanmayiitroorkee17@gmail.com>
2024-05-10 05:57:59 +0530

    updated api

--- client/src/api/authentication.ts
+++ client/src/api/authentication.ts
@@ -1,12 +1,12 @@
-import { userRequest } from "../requestMethods";
+import { publicRequest } from "../requestMethods";
 
 export const login = async (data: any) => {
-    const res = await userRequest.post(`/auth/login`, data);
+    const res = await publicRequest.post(`/auth/login`, data);
     localStorage.setItem("token", res.data.accessToken);
     return res.data;
 };
 
 export const signup = async (data: any) => {
-    const res = await userRequest.post(`/auth/register`, data);
+    const res = await publicRequest.post(`/auth/register`, data);
     return res.data;
 };
--- client/src/requestMethods.ts
+++ client/src/requestMethods.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://task-manager-server-taupe.vercel.app/";
 
 const TOKEN = localStorage.getItem("token");
 // Create axios instance for public requests (without authentication)
