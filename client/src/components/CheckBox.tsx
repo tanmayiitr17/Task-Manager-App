fd68722a6eab52a20511153b673bc2bc7539d589
Tanmay Pandey <tanmayiitroorkee17@gmail.com>
2024-05-10 06:00:18 +0530

    solved vercel error

--- client/src/components/CheckBox.tsx
+++ client/src/components/CheckBox.tsx
@@ -1,7 +1,6 @@
 import { Checkbox } from "@mui/material";
 import { updateTask } from "../api/task";
 import { showError, showMessage } from "../utils/Notify";
-import { useState } from "react";
 
 const CheckBox = ({
     fieldName = "",
@@ -29,7 +28,7 @@ const CheckBox = ({
             }, 1500);
         }
     }
-    
+
     return (
         <div className="ml-[-10px] text-[#000] w-[100%] text-[12px] font-[400] leading-[22px]">
             <Checkbox className="!mb-[4px]" onClick={handleClick} defaultChecked={task?.status} /><span className="text-[#000] text-[14px] font-[500] leading-[22px]">{fieldName}</span>
